c1738beb1e073dce3eb70c09f18295430b3e3f95
Boris Shagiev <boris260692@gmail.com>
2023-02-09 12:26:07 +0200

    Add comments for files

--- src/components/Anonim.js
+++ src/components/Anonim.js
@@ -1,20 +1,22 @@
 import React,{useState}from 'react';
 
 const Anonim = (state) => {
-    console.log(state);
-    const[show,setShow]= useState(false);
+    const[show,setShow]= useState(false);// Перевірка стану додаткового блок при отриманні додаткової інформації контакту
     function createAnonim (){
-        for(let value of Object.values(state.anonim)){
-            if (value.toLowerCase().includes(state.search.toLowerCase().trim()) && state.checkedPerson.checkedPersonAnon === 'finded'){
+        for(let value of Object.values(state.anonim)){ // Перебір значень аноніму
+            if (value.toLowerCase().includes(state.search.toLowerCase().trim()) && state.checkedPerson.checkedPersonAnon === 'finded'){ // Звірка значень аноніма з полями пошуку та значенням чекбоксу
+               // Формую вихідний блок
                 return <div>
                     <div className='wrapper_contact_info'>
                         <p className='contact_info'>{state.anonim.firstName} {state.anonim.lastName}</p>
+                        {/* Кнопка розкриття додаткової інформації та зміни значення show */}
                         <button className='btn_info' onClick={()=>setShow(!show)}>i</button>
                     </div>
+                    {/* якщо значення show true то показую блок */}
                     { show ? <div className='wrapp_contact_deep_info'>
                                 <p>{state.anonim.phone}</p>
                     </div> : null}
-                    <hr noshade className='hr_block'/>
+                    <hr noshade="true" className='hr_block'/>
                 </div>
                 
             }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,22 +2,25 @@ import React, { useState } from 'react';
 import "../styles/Contact.css"
 
 const Contract = (state) => {
-    const[show,setShow]= useState(false);
+    const[show,setShow]= useState(false); // Перевірка стану додаткового блок при отриманні додаткової інформації контакту
     
     function handleSearchChange () {
-        for(let value of Object.values(state.contacts)){
-            for(let valueCheck of Object.values(state.checkedPerson)){
-                if (value.toLowerCase().includes(state.search.toLowerCase().trim()) && valueCheck === state.contacts.gender) {
+        for(let value of Object.values(state.contacts)){ // Перебираю значення ключів контакту
+            for(let valueCheck of Object.values(state.checkedPerson)){ // Перебераю значення чекбоксу
+                if (value.toLowerCase().includes(state.search.toLowerCase().trim()) && valueCheck === state.contacts.gender) { // Роблю звірку значень
+                    // Формую вихідний блок
                     return <div>
                         <div className='wrapper_contact_info'>
                             <p className='contact_info'>{state.contacts.firstName} {state.contacts.lastName}</p>
-                            <button className='btn_info' onClick={()=>setShow(!show)}>i</button>
+                            {/* Кнопка розкриття додаткової інформації та зміни значення show */}
+                            <button className='btn_info' onClick={()=>setShow(!show)}>i</button> 
                         </div>
+                        {/* якщо значення show true то показую блок */}
                         { show ? <div className='wrapp_contact_deep_info'>
                                     <p>{state.contacts.phone}</p>
                                     <p>{state.contacts.gender === "male" ? " стать: чоловіча" : state.contacts.gender !== undefined ? 'стать: жіноча' : null}</p>
                         </div> : null}
-                        <hr noshade className='hr_block'/>
+                        <hr noshade="true" className='hr_block'/>
                     </div>
                 }
             }
